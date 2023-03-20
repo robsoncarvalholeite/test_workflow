64f776ac46c57a3b67d50ca22bea37a78a1b86e6
Robson Leite <robsoncarvalholeite@gmail.com>
2023-03-20 18:13:52 -0300

    Inital commit

---
+++ index.test.js
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+import { bla } from ".";
+
+describe("Teste Inicial", () => {
+  it("Teste 1", () => {
+    const result = bla();
+
+    expect(result).toBeTruthy();
+  });
+});
